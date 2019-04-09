0cc95b8aecc916f72500bb02dbb2b8a8040c00aa
sthobis <s.thobi.sinaga@gmail.com>
2019-04-09 23:27:36 +0700

    feat: add gif cropping

--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@
 
 const Clipper = require("image-clipper");
 const canvas = require("canvas");
+const { GifFrame, GifUtil } = require("gifwrap");
+const Jimp = require("jimp");
 const updateNotifier = require("update-notifier");
 const meow = require("meow");
 
@@ -25,12 +27,52 @@ if (cli.input.length === 0) {
   process.exit(1);
 }
 
-const clipper = Clipper({ canvas });
+const cropGithubGifs = path => {
+  const GIF_WIDTH = 325;
+  const GIF_HEIGHT = 100;
+  const VERTICAL_MARGIN = 53;
+  const HORIZONTAL_MARGIN = 32;
+
+  GifUtil.read(path)
+    .then(source => {
+      let croppedGifs = [];
+
+      for (let i = 0; i < 6; i++) {
+        const isLeft = i % 2 === 0;
+        const x = isLeft ? 0 : GIF_WIDTH + HORIZONTAL_MARGIN;
+        const y = Math.floor(i / 2) * (GIF_HEIGHT + VERTICAL_MARGIN);
+
+        let gif = [];
+        for (let frame = 0; frame < source.frames.length; frame++) {
+          let j = new Jimp(
+            source.frames[frame].bitmap.width,
+            source.frames[frame].bitmap.height,
+            0
+          );
+          j.bitmap.data = source.frames[frame].bitmap.data;
+          j.resize(GIF_WIDTH * 2 + HORIZONTAL_MARGIN, GIF_HEIGHT * 3 + VERTICAL_MARGIN * 2)
+            .crop(x, y, GIF_WIDTH, GIF_HEIGHT);
+          gif[frame] = new GifFrame(j.bitmap);
+        }      
+        croppedGifs.push(gif);
+      }
+
+      croppedGifs.forEach((gif, i) => {
+        GifUtil.quantizeDekker(gif);
+        GifUtil.write(`${i}.gif`, gif).then(result => {
+          console.log(`saved ${i}.gif`);
+        });
+      });
+    })
+    .catch(console.error); 
+};
 
 const WIDTH = 727;
 const INTERVAL = 171;
 
 const cropGithubImages = path => {
+  const clipper = Clipper({ canvas });
+
   for (let i = 0; i < 6; i++) {
     const isLeft = i % 2 === 0;
     const x = isLeft ? 16 : WIDTH - 16 - 325;
@@ -44,4 +86,8 @@ const cropGithubImages = path => {
   }
 };
 
-cropGithubImages(cli.input[0]);
+if (cli.input[0].endsWith(".gif")) {
+  cropGithubGifs(cli.input[0]);
+} else {
+  cropGithubImages(cli.input[0]);
+}
